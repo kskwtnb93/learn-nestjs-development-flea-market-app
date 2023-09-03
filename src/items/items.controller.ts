47ad43c06a1dce1f7048bb86b572651ca4c1c326
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-03 18:55:55 +0900

    ✨ CRUD機能の実装（Create）

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 import { ItemsService } from './items.service';
 
 @Controller('items')
@@ -8,5 +10,23 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.ItemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+
+    return this.ItemsService.create(item);
   }
 }
