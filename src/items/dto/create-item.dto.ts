0c98c96591210a51fda6724c9ddc77982c8b7b0e
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-05 09:47:52 +0900

    ✨ Class Validator 実装

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,6 +1,18 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+
 export class CreateItemDto {
-  id: string;
+  @IsString() // 文字列かどうか
+  @IsNotEmpty() // 1文字以上
+  @MaxLength(40) // 40文字以内
   name: string;
+
+  @IsInt() // 数値
+  @Min(1) // 1円以上
+  @Type(() => Number) // 文字列で渡ってきてもNum型で認識
   price: number;
+
+  @IsString() // 文字列かどうか
+  @IsNotEmpty() // 1文字以上
   description: string;
 }
