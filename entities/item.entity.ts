1694d6e4c7fbeb6e49fd5bf6b0b692d8fb1ace9e
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-11 19:43:39 +0900

    🚧 Entity 修正

--- entities/item.entity.ts
+++ entities/item.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Item {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
