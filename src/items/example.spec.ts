c29db8a74c0274116c2a236e08cdf7f16a57016c
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-17 23:06:50 +0900

    🚨 テストの練習

---
+++ src/items/example.spec.ts
@@ -0,0 +1,15 @@
+describe('Jestの練習', () => {
+  test('Test1', () => {
+    const result = 1;
+    const expected = 1;
+    // result = expected をテスト
+    expect(result).toEqual(expected);
+  });
+
+  // test('Test2', () => {
+  //   const result = 1;
+  //   const expected = 2;
+  //   // result = expected をテスト
+  //   expect(result).toEqual(expected);
+  // });
+});
