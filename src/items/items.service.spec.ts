8e0e96a16e600d549c7251509bb4a600c51d03fb
keisuke watanabe <kskwtnb93@gmail.com>
2023-09-18 00:31:36 +0900

    🚨 findById のユニットテスト

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,10 +1,28 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+
+import { UserStatus } from '../auth/user-status.enum';
+import { ItemStatus } from './item-status.enum';
 import { ItemRepository } from './item.repository';
 import { ItemsService } from './items.service';
 
 const mockItemRepository = () => ({
   find: jest.fn(),
+  findOne: jest.fn(),
 });
+
+const mockUser1 = {
+  id: '1',
+  username: 'test1',
+  password: '1234',
+  status: UserStatus.PREMIUM,
+};
+const mockUser2 = {
+  id: '2',
+  username: 'test2',
+  password: '1234',
+  status: UserStatus.FREE,
+};
 
 describe('ItemsServiceTest', () => {
   let itemsService;
@@ -32,6 +50,33 @@ describe('ItemsServiceTest', () => {
       const result = await itemsService.findAll();
 
       expect(result).toEqual(expected);
+    });
+  });
+
+  describe('findById', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 'test-id',
+        name: 'PC',
+        price: 50000,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: '',
+        updatedAt: '',
+        userId: mockUser1.id,
+        user: mockUser1,
+      };
+      itemRepository.findOne.mockResolvedValue(expected);
+      const result = await itemsService.findById('test-id');
+
+      expect(result).toEqual(expected);
+    });
+
+    it('異常系: 商品が存在しない', async () => {
+      itemRepository.findOne.mockResolvedValue(null);
+      await expect(itemsService.findById('test-id')).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
